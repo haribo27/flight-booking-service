b124448dba85403f5532e13178c898666476c992
Алексей <alexeizubtsoff@yandex.ru>
2025-01-20 15:36:27 +0400

    /

--- CommonDTO/build.gradle.kts
+++ CommonDTO/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.projectlombok:lombok")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
     annotationProcessor("org.projectlombok:lombok")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.4.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- CommonDTO/src/main/java/ru/zubcov/flightbookingservice/commondto/BookingDTO.java
+++ CommonDTO/src/main/java/ru/zubcov/flightbookingservice/commondto/BookingDTO.java
@@ -1,18 +1,28 @@
 package ru.zubcov.flightbookingservice.commondto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Schema(description = "DTO для бронирования")
 public class BookingDTO {
 
+    @Schema(description = "Идентификатор бронирования")
     private Long id;
+    @Schema(description = "Почта пользователя")
     private String userEmail;
+    @Schema(description = "Аэропорт отправления")
     private String departureAirport;
+    @Schema(description = "Аэропорт прибытия")
     private String arrivalAirport;
+    @Schema(description = "Время вылета")
     private LocalDateTime departureTime;
+    @Schema(description = "Время прибытия")
     private LocalDateTime arrivalTime;
+    @Schema(description = "Место в самолете")
     private String seatNumber;
+    @Schema(description = "Статус бронирования")
     private String bookingStatus;
 }
